2c9b8ae7d5d65b8d115226b360ec040dc294e3ff
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-02 17:40:03 -0400

    Partial bring-up for almost all custom ROMs

--- lineage_tokay.mk
+++ lineage_tokay.mk
@@ -6,7 +6,12 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
+ROM_VENDOR := lineage
+ifdef ROM_VENDOR
+$(call inherit-product, vendor/$(ROM_VENDOR)/config/common_full_phone.mk)
+else
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+endif
 
 # Inherit device configuration
 DEVICE_CODENAME := tokay
@@ -19,11 +24,16 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9
+ifdef ROM_VENDOR
+PRODUCT_NAME := $(ROM_VENDOR)_$(DEVICE_CODENAME)
+else
 PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+endif
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2424
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tokay-user 16 BP3A.251005.004.B1 14042072 release-keys" \
