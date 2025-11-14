CalyxOS/device_google_caimito

caimito: Switch to our own kernel
Change-Id: Iaec4d3704b2b44cd809f24d922f6965312da71e5

device-caiman.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_CAIMAN_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_CAIMAN_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_CAIMAN_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/calyx
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := $(RELEASE_GOOGLE_CAIMAN_16K_DEVELOPER_OPTION)
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 LOCAL_PATH := device/google/caimito

device-komodo.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_KOMODO_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_KOMODO_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_KOMODO_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/calyx
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := $(RELEASE_GOOGLE_KOMODO_16K_DEVELOPER_OPTION)
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 LOCAL_PATH := device/google/caimito

device-tokay.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_TOKAY_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_TOKAY_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_TOKAY_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/calyx
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := $(RELEASE_GOOGLE_TOKAY_16K_DEVELOPER_OPTION)
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 # display

Trijal08/android_device_google_caimito

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

device-caiman.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_CAIMAN_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_CAIMAN_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_CAIMAN_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/lineage
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := true
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 LOCAL_PATH := device/google/caimito

device-komodo.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_KOMODO_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_KOMODO_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_KOMODO_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/lineage
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := true
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 LOCAL_PATH := device/google/caimito

device-tokay.mk
@@ -32,17 +32,11 @@ else
 TARGET_LINUX_KERNEL_VERSION ?= 6.1
 endif
 
-ifdef RELEASE_KERNEL_TOKAY_DIR
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_TOKAY_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_TOKAY_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/caimito-kernels/lineage
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
 
 ifneq ($(TARGET_BOOTS_16K),true)
 PRODUCT_16K_DEVELOPER_OPTION := true
-endif
-
-else
-TARGET_KERNEL_DIR ?= device/google/caimito-kernels/6.1/24D1
-TARGET_BOARD_KERNEL_HEADERS ?= device/google/caimito-kernels/6.1/24D1/kernel-headers
 endif
 
 # display